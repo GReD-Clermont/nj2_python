93ade522ded1389bff1db97ebab522d616fc630e
SANTE\gumougeo <guillaume.mougeot@laposte.net>
2023-09-05 12:36:43 +0200

    added converter

--- compute_params.py
+++ compute_params.py
@@ -12,7 +12,6 @@ def install(packages):
 try:
     import tifffile as tiff
     from skimage import measure
-    from skimage.transform import resize
     from skimage import io
     from scipy.spatial import Delaunay
     import numpy as np
@@ -23,7 +22,6 @@ except ImportError as e:
     install(['tifffile', 'scikit-image', 'scipy', 'numpy', 'pandas'])
     import tifffile as tiff
     from skimage import measure
-    from skimage.transform import resize
     from skimage import io
     from scipy.spatial import Delaunay
     import numpy as np
@@ -145,124 +143,7 @@ def imread(img_path):
         print("[Error] Unknown image format:", extension)
 
 #---------------------------------------------------------------------------
-# 3D resizer
-
-def resize_segmentation(segmentation, new_shape, order=3):
-    '''
-    Copied from batch_generator library. Copyleft Fabian Insensee.
-    Resizes a segmentation map. Supports all orders (see skimage documentation). Will transform segmentation map to one
-    hot encoding which is resized and transformed back to a segmentation map.
-    This prevents interpolation artifacts ([0, 0, 2] -> [0, 1, 2])
-    :param segmentation:
-    :param new_shape:
-    :param order:
-    :return:
-    '''
-    tpe = segmentation.dtype
-    unique_labels = np.unique(segmentation)
-    assert len(segmentation.shape) == len(new_shape), "new shape must have same dimensionality as segmentation"
-    if order == 0:
-        return resize(segmentation.astype(float), new_shape, order, mode="edge", clip=True, anti_aliasing=False).astype(tpe)
-    else:
-        reshaped = np.zeros(new_shape, dtype=segmentation.dtype)
-
-        for i, c in enumerate(unique_labels):
-            mask = segmentation == c
-            reshaped_multihot = resize(mask.astype(float), new_shape, order, mode="edge", clip=True, anti_aliasing=False)
-            reshaped[reshaped_multihot >= 0.5] = c
-        return reshaped
-
-def resize_3d(img, output_shape, order=3, is_msk=False, monitor_anisotropy=True, anisotropy_threshold=3):
-    """
-    Resize a 3D image given an output shape.
-    
-    Parameters
-    ----------
-    img : numpy.ndarray
-        3D image to resample.
-    output_shape : tuple, list or numpy.ndarray
-        The output shape. Must have an exact length of 3.
-    order : int
-        The order of the spline interpolation. For images use 3, for mask/label use 0.
-
-    Returns
-    -------
-    new_img : numpy.ndarray
-        Resized image.
-    """
-    assert len(img.shape)==4, '[Error] Please provided a 3D image with "CWHD" format'
-    assert len(output_shape)==3 or len(output_shape)==4, '[Error] Output shape must be "CWHD" or "WHD"'
-    
-    # convert shape to array
-    input_shape = np.array(img.shape)
-    output_shape = np.array(output_shape)
-    if len(output_shape)==3:
-        output_shape = np.append(input_shape[0],output_shape)
-    if np.all(input_shape==output_shape): # return image if no reshaping is needed
-        return img 
-        
-    # resize function definition
-    resize_fct = resize_segmentation if is_msk else resize
-    resize_kwargs = {} if is_msk else {'mode': 'edge', 'anti_aliasing': False}
-        
-    # separate axis --> [Guillaume] I am not sure about the interest of that... 
-    # we only consider the following case: [147,512,513] where the anisotropic axis is undersampled
-    # and not: [147,151,512] where the anisotropic axis is oversampled
-    anistropy_axes = np.array(input_shape[1:]) / input_shape[1:].min()
-    do_anisotropy = monitor_anisotropy and len(anistropy_axes[anistropy_axes>anisotropy_threshold])==2
-    if not do_anisotropy:
-        anistropy_axes = np.array(output_shape[1:]) / output_shape[1:].min()
-        do_anisotropy = monitor_anisotropy and len(anistropy_axes[anistropy_axes>anisotropy_threshold])==2
-        
-    do_additional_resize = False
-    if do_anisotropy: 
-        axis = np.argmin(anistropy_axes)
-        print("[resize] Anisotropy monitor triggered! Anisotropic axis:", axis)
-        
-        # as the output_shape and the input_shape might have different dimension
-        # along the selected axis, we must use a temporary image.
-        tmp_shape = output_shape.copy()
-        tmp_shape[axis+1] = input_shape[axis+1]
-        
-        tmp_img = np.empty(tmp_shape)
-        
-        length = tmp_shape[axis+1]
-        tmp_shape = np.delete(tmp_shape,axis+1)
-        
-        for c in range(input_shape[0]):
-            coord  = [c]+[slice(None)]*len(input_shape[1:])
-
-            for i in range(length):
-                coord[axis+1] = i
-                tmp_img[tuple(coord)] = resize_fct(img[tuple(coord)], tmp_shape[1:], order=order, **resize_kwargs)
-            
-        # if output_shape[axis] is different from input_shape[axis]
-        # we must resize it again. We do it with order = 0
-        if np.any(output_shape!=tmp_img.shape):
-            do_additional_resize = True
-            order = 0
-            img = tmp_img
-        else:
-            new_img = tmp_img
-    
-    # normal resizing
-    if not do_anisotropy or do_additional_resize:
-        new_img = np.empty(output_shape)
-        for c in range(input_shape[0]):
-            new_img[c] = resize_fct(img[c], output_shape[1:], order=order, **resize_kwargs)
-            
-    return new_img
-
-#---------------------------------------------------------------------------
 # Utils for volume meshing
-
-def get_resample_shape(input_shape, spacing, median_spacing):
-    input_shape = np.array(input_shape)
-    spacing = np.array(spacing)
-    median_spacing = np.array(median_spacing)
-    if len(input_shape)==4:
-        input_shape=input_shape[1:]
-    return np.round(((spacing/median_spacing)[::-1]*input_shape)).astype(int)
 
 def mesh3d(verts, img, remove_label=0):
     """Creates a 3D mesh from a list of vertices. Uses the image as a mask to remove unwanted triangles.
@@ -299,7 +180,7 @@ def compute_sphericity(volume, surface):
 def safe_imread(img_path, spacing=()):
     # read image
     img, metadata = imread(img_path)
-    if len(metadata['spacing'])!=0 and len(spacing)==0: spacing = np.array(metadata['spacing'])
+    if len(metadata['spacing'])==3 and len(spacing)==0: spacing = np.array(metadata['spacing'])
 
     assert len(img.shape)==3 or (len(img.shape)==4 and img.shape[0]==1), "[Error] Strange image shape ({}). Please provide a 3d image".format(img.shape)
     
@@ -313,14 +194,8 @@ def safe_imread(img_path, spacing=()):
     # warning if no spacing
     if len(spacing)==0:
         print("[Warning] No spacing has been defined. The result will be expressed in voxel units.")
-    else:
-        # eventually resample image
-        output_shape = get_resample_shape(img.shape, spacing/spacing.min(), [1.,1.,1.])
-        if len(img.shape)==3:
-            img = np.expand_dims(img, 0)
-        img = resize_3d(img, output_shape, order=1, is_msk=True)[0]
     
-    return img, metadata
+    return img, spacing
 
 def compute_volume_surface_sphericity(img, bg=None, spacing=(), verbose=False):
     """Compute volume, surface and sphericity of a volumetric object.
@@ -417,15 +292,18 @@ def compute_flatness_elongation(img, bg=None, spacing=(), verbose=False):
     # compute barycenter and center the foreground voxels
     bary = np.mean(fg,axis=0)
     fg = fg - bary
-
-    if len(spacing)>0:
-        fg = fg*np.array(spacing)
+    # fg = fg / np.sqrt(np.mean(fg*fg))
 
     # get the covariance matrix
     cov = fg.T.dot(fg)/len(fg)
 
+    # eventually resize image
+    if len(spacing)>0:
+        spacing = np.array(spacing).reshape(1,3)
+        cov = cov*(spacing.T.dot(spacing))
+
     # get the eigenvalues
-    eigval = sorted(np.linalg.eig(cov)[0])
+    eigval = np.real(sorted(np.linalg.eig(cov)[0]))
 
     # compute flatness and elongation
     flatness = np.sqrt(eigval[1]/eigval[0])
@@ -461,6 +339,7 @@ def compute_number_vmean_vtot(img, cc_img, bg=None, spacing=(), verbose=False):
     vmean = np.mean(vol_voxel[unq!=bg])
     vtot = np.sum(vol_voxel[unq!=bg])
 
+    # No need because images are already resampled!
     # set the spacing if needed
     if len(spacing)>0:
         vmean = vmean*np.prod(spacing)
@@ -511,11 +390,11 @@ class ComputeParams:
         if verbose: print("Background voxel is set to", bg)
 
         # read nucleus image and metadata
-        self.nc_imag, self.nc_meta = safe_imread(img_path=self.nc_path, spacing=self.spacing)
+        self.nc_imag, self.spacing = safe_imread(img_path=self.nc_path, spacing=self.spacing)
 
         # read chromocenter image and metadata
         if cc_path is not None:
-            self.cc_imag, self.cc_meta = safe_imread(img_path=cc_path, spacing=self.spacing)
+            self.cc_imag, _ = safe_imread(img_path=cc_path, spacing=self.spacing)
 
         # nucleus volume, surface and sphericity computation
         self.nc_params['volume'], self.nc_params['surface'], self.nc_params['sphericity'] = compute_volume_surface_sphericity(self.nc_imag, bg=bg, spacing=self.spacing, verbose=verbose)
@@ -556,7 +435,9 @@ def compute_directory(path, cc_path=None, bg=0, spacing=(), out_path="params.csv
     for i in range(len(filenames)):
         print("[{}/{}] Computing parameters for {}".format(i,len(filenames),filenames[i]))
         img_path = os.path.join(path, filenames[i])
-        cc_img_path = os.path.join(cc_path, filenames[i])
+        if cc_path is not None: cc_img_path = os.path.join(cc_path, filenames[i])
+        else: cc_img_path = None
+        if not os.path.exists(cc_img_path): cc_img_path = None
 
          # compute parameters for that image
         comp_params = ComputeParams(nc_path=img_path, cc_path=cc_img_path, bg=bg, spacing=spacing, verbose=verbose)
--- generate_sphere.py
+++ generate_sphere.py
@@ -1,5 +1,5 @@
 import numpy as np
-from skimage.io import imsave
+from tifffile import imwrite, transpose_axes
 import os
 
 img = np.zeros((100,100,100))
@@ -9,31 +9,65 @@ center = np.array(center)
 
 ray = 50
 
-def gen_sphere(img, center, ray):
+def gen_sphere(img, center, ray, return_raw=False):
     x,y,z = np.meshgrid(*[np.arange(d) for d in img.shape])
     x,y,z = x-center[0],y-center[1],z-center[2]
-    img[np.sqrt(x*x+y*y+z*z)<ray]=1
-    return img
+    if return_raw:
+        msk = np.copy(img)
+        img=np.maximum(ray-np.sqrt(x*x+y*y+z*z),0)
+        msk[np.sqrt(x*x+y*y+z*z)<ray]=1
+        return img, msk
+    else:
+        img[np.sqrt(x*x+y*y+z*z)<ray]=1
+        return img
 
-def alea_sphere(img_size, range_ray, range_center):
+def alea_sphere(img_size, range_ray, range_center, return_raw=False):
     img_size = np.array(img_size)
     range_ray = np.array(range_ray)
     range_center = np.array(range_center)
+
     ray = np.random.randint(range_ray[0], range_ray[1])
     center = np.array([np.random.randint(rc[0], rc[1]) for rc in range_center])
     center = np.where(np.greater(center + ray,img_size), img_size-ray, center)
     center = np.where(np.less(center-ray, 0), ray, center)
-    img = np.zeros(img_size)
-    return gen_sphere(img, center, ray), 4*np.pi*(ray**3)/3
 
-def sphere_dir(path, n=20):
+    img = np.zeros(img_size)
+
+    return *gen_sphere(img, center, ray, return_raw=return_raw), 4*np.pi*(ray**3)/3
+
+def sphere_dir(path, path_msk, n=20):
+    os.makedirs(path, exist_ok=True)
+    os.makedirs(path_msk, exist_ok=True)
+
     for i in range(n):
-        msk, vol = alea_sphere((100,100,100),(10,30),((20,80),(20,80),(20,80)))
-        imsave(os.path.join(path, str(vol)+'.tif'), msk)
+        img, msk, vol = alea_sphere((100,100,100),(10,30),((20,80),(20,80),(20,80)), return_raw=True)
+
+        img = (img * 255 / img.max()).astype(int)
+        msk = (msk * 255 / msk.max()).astype(int)
+
+        img = transpose_axes(img, 'ZYX', 'TZCYXS')
+        msk = transpose_axes(msk, 'ZYX', 'TZCYXS')
+
+        res = ((10320,1000),(10320,1000),'MICROMETER')
+        metadata = {'spacing':0.2, 'unit':'MICROMETER'}
+        vol = vol * 0.1032 * 0.1032 * 0.2
+
+        imwrite(os.path.join(path, str(vol)+'.tif'), img.astype(np.uint8),
+            resolution=res,
+            imagej=True, 
+            metadata=metadata,
+            compression=('zlib', 1))
+        imwrite(os.path.join(path_msk, str(vol)+'.tif'), msk.astype(np.uint8),
+            resolution=res,
+            imagej=True, 
+            metadata=metadata,
+            compression=('zlib', 1))
         print("Measured volume:", np.sum(msk))
         print("Actual volume:", vol)
 
-sphere_dir("img\\")
+
+
+sphere_dir("data\\img", "data\\msk")
 # msk = gen_sphere(img, center, ray)
 # msk, vol = alea_sphere((100,100,100),(10,30),((20,80),(20,80),(20,80)))
 # imsave("tmp.tif", msk)
